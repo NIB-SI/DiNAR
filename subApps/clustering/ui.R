39755b1087f8a3bf08105a030331dec0b2574f82
zagorGit <maja.zagorscak@nib.si>
2018-01-28 14:25:37 +0100

    Add files via upload

---
+++ subApps/clustering/ui.R
@@ -0,0 +1,52 @@
+
+
+library(shiny)
+
+# fluidPage(
+  navbarPage("DiNARsubApp",
+             
+    tabPanel("Intro", 
+             htmlOutput("text0")
+             ),
+             
+  tabPanel(".graphml",
+           titlePanel("Uploading Files"),
+           sidebarLayout(
+             sidebarPanel(
+               fileInput('file3', 'Choose .graphml File',
+                         accept = c('.graphml')),
+               tags$hr(),
+               textInput("caption2", "Type in desired network name:", "CustomNetwork"),
+               verbatimTextOutput("value2"),
+               tags$hr(),
+               actionButton("go2", "Go!")
+             ),
+             mainPanel(
+               tabsetPanel(
+                 tabPanel("Nodes",
+                          conditionalPanel(
+                            condition = "(input.go2!=0)",
+                            dataTableOutput('nodesNEW2')),
+                          conditionalPanel(
+                            condition = "(input.go2!=0)",
+                            downloadButton("downloadNodesG", "Download")
+                          )
+                 ),
+                 tabPanel("Edges",
+                          conditionalPanel(
+                            condition = "(input.go2!=0)",
+                            dataTableOutput('edgesNEW2')),
+                          conditionalPanel(
+                            condition = "(input.go2!=0)",
+                            downloadButton("downloadEdgesG", "Download")
+                          )
+                 )
+               )
+             )
+           )
+  )
+  
+
+)
+# )
+
